50b985ac150cd4f361d21a5045de6a09f8580560
vinothtcb <43381714+vinothtcb@users.noreply.github.com>
2026-09-25 12:00:53 +0530

    Delete thalavadi-backend directory

--- thalavadi-backend/src/routes/searchRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { globalSearch } = require("../controllers/searchController");
-
-const router = express.Router();
-
-router.get("/", globalSearch);
-
-module.exports = router;
--- thalavadi-backend/thalavadi-backend/src/routes/weatherRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getWeather } = require("../controllers/weatherController");
-
-const router = express.Router();
-
-router.get("/", getWeather);
-
-module.exports = router;
